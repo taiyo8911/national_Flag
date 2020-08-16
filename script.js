ccd0e73b8d01385c7614ff0599ada76f934b3248
taiyo8911 <68884687+taiyo8911@users.noreply.github.com>
2020-08-16 18:22:00 +0900

    first commit.

---
+++ script.js
@@ -0,0 +1,19 @@
+function show1() {
+    document.area.src = "image/china-national-flag.PNG";
+    document.getElementById("area").href = "https://ja.wikipedia.org/wiki/%E4%B8%AD%E8%8F%AF%E4%BA%BA%E6%B0%91%E5%85%B1%E5%92%8C%E5%9B%BD";
+}
+
+function show2() {
+    document.area.src = "image/india-national-flag.PNG";
+    document.getElementById("area").href = "https://ja.wikipedia.org/wiki/%E3%82%A4%E3%83%B3%E3%83%89";
+}
+
+function show3() {
+    document.area.src = "image/thailand-national-flag.PNG";
+    document.getElementById("area").href = "https://ja.wikipedia.org/wiki/%E3%82%BF%E3%82%A4%E7%8E%8B%E5%9B%BD";
+}
+
+function show4() {
+    document.area.src = "image/russia-national-flag.PNG";
+    document.getElementById("area").href = "https://ja.wikipedia.org/wiki/%E3%83%AD%E3%82%B7%E3%82%A2";
+}
